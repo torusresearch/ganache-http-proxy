4596abb6c7b0d77be4d0a7fd3b14264276cd06a1
tetratorus <tetratorus@gmail.com>
2019-05-18 18:16:23 -0400

    Add more helpful logs

--- server.js
+++ server.js
@@ -3,14 +3,18 @@
 
 var fs = require('fs');
 var httpProxy = require('http-proxy');
-
-httpProxy.createServer({
-  target: {
-    host: 'localhost',
-    port: 8546
-  },
-  ssl: {
-    key: fs.readFileSync('server.key', 'utf8'),
-    cert: fs.readFileSync('server.cert', 'utf8')
-  }
-}).listen(8545);
+console.log("This proxy server routes https://localhost:8545 traffic to http://localhost:8546")
+try {
+  httpProxy.createServer({
+    target: {
+      host: 'localhost',
+      port: 8546
+    },
+    ssl: {
+      key: fs.readFileSync('server.key', 'utf8'),
+      cert: fs.readFileSync('server.cert', 'utf8')
+    }
+  }).listen(8545);
+} catch (e) {
+  console.log("Error occured, is ganache running on port 8546?")
+}
