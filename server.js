42c8fcf3deccd75fdc715f48a125abcab5f0b0ab
chaitanyapotti <chaitanya.potti@gmail.com>
2019-08-02 10:07:32 +0530

    Using mkcert to create CA

--- server.js
+++ server.js
@@ -1,20 +1,40 @@
 #!/usr/bin/env node
 
+var httpProxy = require("http-proxy");
+const mkcert = require("mkcert");
 
-var fs = require('fs');
-var httpProxy = require('http-proxy');
-console.log("This proxy server routes https://localhost:8545 traffic to http://localhost:8546")
+console.log("This proxy server routes https://localhost:8545 traffic to http://localhost:8546");
 try {
-  httpProxy.createServer({
-    target: {
-      host: 'localhost',
-      port: 8546
-    },
-    ssl: {
-      key: fs.readFileSync('server.key', 'utf8'),
-      cert: fs.readFileSync('server.cert', 'utf8')
-    }
-  }).listen(8545);
+  mkcert
+    .createCA({
+      organization: "Torus Labs",
+      countryCode: "SG",
+      state: "Singapore",
+      locality: "Singapore",
+      validityDays: 365
+    })
+    .then(ca =>
+      mkcert.createCert({
+        domains: ["127.0.0.1", "localhost"],
+        validityDays: 365,
+        caKey: ca.key,
+        caCert: ca.cert
+      })
+    )
+    .then(cert => {
+      httpProxy
+        .createServer({
+          target: {
+            host: "localhost",
+            port: 8546
+          },
+          ssl: {
+            key: cert.key,
+            cert: cert.cert
+          }
+        })
+        .listen(8545);
+    });
 } catch (e) {
-  console.log("Error occured, is ganache running on port 8546?")
+  console.log("Error occured, is ganache running on port 8546?");
 }
